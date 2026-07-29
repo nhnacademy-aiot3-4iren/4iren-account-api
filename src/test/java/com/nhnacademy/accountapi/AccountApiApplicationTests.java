c8f61e07fba5b12a98cd351b8f79c63973d30a94
조선대-NHN 29 <jun03031@gmail.com>
2026-07-29 17:47:43 +0900

    fix: dummy test 파일 작성

---
+++ src/test/java/com/nhnacademy/accountapi/AccountApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.accountapi;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
