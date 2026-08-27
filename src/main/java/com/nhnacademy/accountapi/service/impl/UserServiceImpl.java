6e521f3011f89d4ae303ad72efc7a747e593b5ca
shinjunsik <jun03031@gmail.com>
2026-08-27 09:08:06 +0900

    fix: rabbitmq dto 수정

--- src/main/java/com/nhnacademy/accountapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/nhnacademy/accountapi/service/impl/UserServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 
@@ -203,7 +204,8 @@ public class UserServiceImpl implements UserService {
         RoleChangeMessage roleChangeMessage = new RoleChangeMessage(
                 userId,
                 role.toUpperCase(),
-                jti
+                jti,
+                LocalDateTime.now()
         );
 
         rabbitTemplate.convertAndSend(
